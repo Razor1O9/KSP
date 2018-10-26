c0bc64e64c9525af683fec4d32531a4d51600ba6
Unknown <yassirx91@googlemail.com>
2018-10-26 13:45:19 +0200

    new function cluster

--- njvm.c
+++ njvm.c
@@ -19,30 +19,60 @@
 #define IMMEDIATE(x) ((x) & 0x00FFFFFF)
 #define SIGN_EXTEND(i) ((i) & 0x00800000 ? (i) | 0xFF000000 : (i))
 
-int programMemory[9999]; // Befehlsliste
-int pc; // program counter
+int version = 1; // The current version
+int programMemory[9999]; // List of all Program Instructions
+int pc; // Program Counter Variable for Instructions
 
-int calculationStack[9999]; // Rechnerstack
-int si; // stack index
+int calculationStack[9999]; // Stack for Calculation
+int sp; // Stack Pointer Variable for Calculations
 
-unsigned int zahl; // natürliche Zahl
+unsigned int source[9999]; // natürliche Zahl
 char letter; // character
 
 
+/**
+ * Main Function, which reads all Terminal Arguments
+ * Valid Arguments start more specific VM functions
+ * @param argcount
+ * @param argvector
+ * @return
+ */
 int main(int argcount, char *argvector[]) {
 
     printf("Ninja Virtual Machine started\n");
     for (argcount = 0; argcount < argvector; argcount++) {
         if (!strcmp(programMemory[argcount], "--version")) {
-            printf("Version 1\n");
+            printf("Version = ", version, "\n");
         } else if (!strcmp(programMemory[argcount], "--help")) {
-            printf("Valid inputs: \n --version \n --help\n");
+            printf("Valid inputs: \n --version \n --help\n --program1 \n --program2 \n --program3 \n");
         } else if (!strcmp(programMemory[argcount], "1")) {
-            printf("... \n");
+            source[0] = PUSHC | IMMEDIATE(3);
+            source[1] = PUSHC | IMMEDIATE(4);
+            source[2] = ADD;
+            source[3] = PUSHC | IMMEDIATE(10);
+            source[4] = PUSHC | IMMEDIATE(6);
+            source[5] = SUB;
+            source[6] = MUL;
+            source[7] = WRINT;
+            source[8] = PUSHC | IMMEDIATE(10);
+            source[9] = WRCHR;
+            source[10] = HALT;
         } else if (!strcmp(programMemory[argcount], "2")) {
-            printf("... \n");
+            source[0]=PUSHC|IMMEDIATE(-2);
+            source[1]=RDINT;
+            source[2]=MUL;
+            source[3]=PUSHC|IMMEDIATE(3);
+            source[4]=ADD;
+            source[5]=WRINT;
+            source[6]=PUSHC|'\n';
+            source[7]=WRCHR;
+            source[8]=HALT;
         } else if (!strcmp(programMemory[argcount], "3")) {
-            printf("... \n");
+            source[0]=RDCHR;
+            source[1]=WRINT;
+            source[2]=PUSHC|'\n';
+            source[3]=WRCHR;
+            source[4]=HALT;
         }
     }
     printf("End of input reached\n");
@@ -50,48 +80,59 @@ int main(int argcount, char *argvector[]) {
     return EXIT_SUCCESS;
 }
 
-
+/**
+ *
+ * @param opCode
+ */
 void calculate(int opCode) {
     if (opCode == ADD) {
-        calculationStack[si - 1] = calculationStack[si - 1] + calculationStack[si];
+        calculationStack[sp - 1] = calculationStack[sp - 1] + calculationStack[sp];
     }
     if (opCode == SUB) {
-        calculationStack[si - 1] = calculationStack[si - 1] - calculationStack[si];
+        calculationStack[sp - 1] = calculationStack[sp - 1] - calculationStack[sp];
     }
     if (opCode == MUL) {
-        calculationStack[si - 1] = calculationStack[si - 1] * calculationStack[si];
+        calculationStack[sp - 1] = calculationStack[sp - 1] * calculationStack[sp];
     }
     if (opCode == DIV) {
-        calculationStack[si - 1] = calculationStack[si - 1] / calculationStack[si];
+        calculationStack[sp - 1] = calculationStack[sp - 1] / calculationStack[sp];
     }
     if (opCode == MOD) {
-        calculationStack[si - 1] = calculationStack[si - 1] / calculationStack[si];
+        calculationStack[sp - 1] = calculationStack[sp - 1] / calculationStack[sp];
     }
 }
 
+/**
+ *
+ * @param opCode
+ */
 void data(int opCode) {
     if (opCode == RDINT) {
         // zahl einlesen
     }
     if (opCode == WRINT) {
-        printf(zahl);
-        si--;
+        printf(source);
+        sp--;
     }
     if (opCode == RDCHR) {
         // char einlesen
     }
     if (opCode == WRCHR) {
         printf(letter);
-        si--;
+        sp--;
     }
 }
 
+/**
+ *
+ * @return
+ */
 int matchInstruction() {
     for (pc = 0; pc < programMemory; pc++) {
         if (programMemory[pc] == PUSHC) {
             pc++;
-            zahl = programMemory[pc];
-            calculationStack[pc] = zahl;
+            source = programMemory[pc];
+            calculationStack[pc] = source;
         } else if (programMemory[pc] == HALT) {
             exit;
             printf("HALT \n");

9b6f67edc4d88da18b29c6a1cee873fe6b8328ef
Duc <duc1991_2011@yahoo.de>
2019-07-23 17:27:50 +0200

    alter prog2.asm

--- prog2.asm
+++ prog2.asm
@@ -1,10 +1,39 @@
-pushc	1000000
-pushc	1000000
-mul 
-pushc	2000000
-pushc	2000000
-mul
-ge
-pushc	10
-wrchr
-halt
+	rdint
+	popg	0
+	// y = readInteger();
+	rdint
+	popg	1
+	// while ...
+L1:
+	// x != y
+	pushg	0
+	pushg	1
+	ne
+	brf	L2
+	// if ...
+	pushg	0
+	pushg	1
+	gt
+	brf	L3
+	// x = x - y
+	pushg	0
+	pushg	1
+	sub
+	popg	0
+	jmp	L4
+L3:
+	// y = y - x
+	pushg	1
+	pushg	0
+	sub
+	popg	1
+L4:
+	jmp	L1
+L2:
+	// writeInteger(x);
+	pushg	0
+	wrint
+	// writeCharacter('\n');
+	pushc	'\n'
+	wrchr
+	halt

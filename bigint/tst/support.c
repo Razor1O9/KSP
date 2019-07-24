192c99311555be8aa6d5bf07459943adf79748d3
David <david_omran@yahoo.de>
2019-07-24 09:55:38 +0200

    library

--- bigint/tst/support.c
+++ bigint/tst/support.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "support.h"
+#include "bigint/build/include/support.h"
 
 
 /*
--- bigint/tst/testbip.c
+++ bigint/tst/testbip.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "bigint.h"
+#include "bigint/build/include/bigint.h"
 
 
 /**************************************************************/
